046ad56610e2d402341790c05f3caa4267fb1863
sm <a0086845@u.nus.edu>
2018-01-11 16:32:08 +0800

    added iframe for video in experty

--- src/components/reviews/Experty.js
+++ src/components/reviews/Experty.js
@@ -3,6 +3,14 @@ import React from "react"
 const Experty = () => (
   <div>
     <h1>Experty Token Review</h1>
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/r-iLWMTc354"
+      frameBorder="0"
+      allow="autoplay; encrypted-media"
+      allowFullScreen
+    />
   </div>
 )
 
