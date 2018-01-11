6c05bd60c8c7d56ec9ec723dfaea779a6e60800c
sm <a0086845@u.nus.edu>
2018-01-11 15:52:34 +0800

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react"
+import "./App.css"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Bite-Crypto</h1>
+        </header>
+        <p className="App-intro">This is the main page</p>
+      </div>
+    )
+  }
+}
+
+export default App
