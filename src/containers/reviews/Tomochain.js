94855d4e94b8c25fb1e3a748f13d549dd4e244ac
sm <a0086845@u.nus.edu>
2018-02-12 11:06:46 +0800

    updated tomochain content

--- src/containers/reviews/Tomochain.js
+++ src/containers/reviews/Tomochain.js
@@ -29,33 +29,98 @@ const Tomochain = () => (
 
     <ReactMarkdown
       source={`
-### TomoChain
+## TomoChain
 
-Hi guys, welcome to another episode of Bite Crypto, where we provide Bite sized crypto news to satisfy your daily crypto needs. If you have been liking our videos so far, remember to spam the like button and click to subscribe to get updates on our latest videos.
+Website: https://tomocoin.io/
 
-For today’s video, we will be conducting a review for Tomo Coin ICO. Tomo Coin is basically a digital currency that will be used in the Tomo ecosystem which consists of 3 parts : the Tomochain, the Tomowallet, and apps built on top of Tomo Chain. The Tomochain testnet, demo Tomo Wallet and beta Tomoapp (a decentralized knowledge sharing platform) are all already available and you can check them out using the links in the description box below.
+Whitepaper: https://drive.google.com/file/d/1EAs7llLOpQx3YH1onvC4Qb107sIiztcm/view
 
-The Tomo ecosystem is built on Tomochain, which is the underlying blockchain with the potential to process billions of transactions per day with near zero fees and almost instantaneous transaction confirmation. Tomochain aims to solve the scalability problems currently faced by the Ethereum network by reducing the confirmation time from 15 seconds average for Ethereum to 2 seconds which is 7 times faster than Ethereum by utilising the proof of authority (POA) consensus method. Tomocoin will be the digital currency of this blockchain and used as block rewards for nodes.
+### Project Summary
 
-Tomochain will have the same features as Ethereum which allows for supporting smart contracts and also the allowance for token issuance. Furthermore, it is fully compatible with Ethereum smart contracts which basically means that existing projects on Ethereum can easily migrate onto TomoChain.
+Tomocoin is a currency that will be used in the Tomo ecosystem, which consists of 3 parts : Tomochain, Tomowallet, and Tomoapp. Through these 3 parts, the Tomocoin team aims to build the Internet of Value, creating a financial system which is more secure, transparent, efficient, inclusive and equitable for everyone.
 
-### Initial Coin Offering (ICO)
+### Token Sales
 
-Tomo Chain ICO will have a total hard cap of USD 8.5 million and each Tomocoin will be worth 0.25 USD at launch. The ICO date is slated for March 2018 with a presale cap of USD 7.5 million and crowd sale cap of USD 1 million. Take note that citizens in the US, Canada, UK, Singapore and Hong Kong are not allowed to take part in this ICO.
+Token Ticker: TMC
+Total Supply of Tokens: 100,000,000
+ICO Date: TBA (March 2018)
+Proof of Care + KYC Submission: 12th Feb 2018, 12pm Singapore (GMT +8)
+Presale Cap: $7,500,000 USD
+Hard cap at ICO: $1,000,000 USD
+Total Cap: 8,500,000 USD
+Price per Token: 0.25 USD
+Bonuses: Up to 20% bonus for partners
+Countries excluded: US, China, Taiwan, Singapore, Hong Kong
 
-ICO Website: https://www.tomocoin.io/
+### Product
+1. **Tomochain**
+- Tomochain is the underlying blockchain which forms the backbone of the Tomo ecosystem. It’s aim is to be an open-source blockchain infrastructure framework that can process billions of financial transactions per day with near zero fees and almost instantaneous transaction confirmation.
 
-### Short Term
+- Tomochain is a side-chain on the Ethereum network with support for horizontal scaling by adding second-layer blockchains, and also allow for cross-chain atomic transfers. It aims to solve the scalability problems currently faced by the Ethereum network. Tomocoin will be the currency of this blockchain and used as block rewards for nodes. With a 2 second confirmation time, Tomochain would be able to process more transactions per second than Ethereum.
 
-Bite Crypto believes that this project is a good short term investment as the Ethereum network currently faces congestion issues on a regular basis (especially during ICO periods). The Tomo ecosystem should prove to be a good solution that many projects and users will consider moving into. With Long Vuong, co-founder of New economic movement, currently one of the top 10 cryptocurrencies in the world at the helm of the experienced Tomo Chain team, the backing of notable advisors and proof of a working product, all indicators point that the project will be able to deliver as per their schedule which is the official release of the tomoapp in q3 2018 and the official release of tomochain in q4 2018. Also, considering the scope of the project, and the low hard cap for ICO, we believe Tomochain definitely will get off to a good start.
+- Tomochain also supports smart contracts and allows for token issuance, and is fully compatible with Ethereum smart contracts. This means that new projects can choose to ICO and issue tokens on Tomochain, and existing projects can migrate onto Tomochain as well. We believe this will be an extremely attractive proposition for projects that need high throughput and low transaction fees.
+Furthermore, the team has already built the first Tomo dApp, aptly named Tomoapp, to showcase the capabilities of its network. The Tomochain testnet is available at https://stats.TomoCoin.io/
 
-### Long Term
+- Tomochain will launch with a Proof of Authority(PoA) consensus, and has plans in place to switch to Masternode architecture some time in 2019.
 
-As for the long term outlook, Bite Crypto is neutral on Tomo Chain. There is little detail available regarding the future direction of Tomocoin as their roadmap is not very extensive after 2018. Furthermore, to ensure its long term success, Tomochain needs to have its own unique and differentiating characteristics from the Ethereum network, in order to maintain its userbase.
 
----------------
+2. **Tomoapp:**
+- Tomoapp is decentralized knowledge sharing application on Tomochain with thousands of weekly users. It consists of an incentive system that motivates users to contribute and work together to build a efficient knowledge sharing network. Users are encouraged to ask questions and other users can choose to answer these questions, earning Tomocoins in the process. Furthermore, the team intends to have more advanced features in future, such as creating a video channel in the app and charging users to access the channel, or gifting Tomocoins to live streamers.  The Beta version is now available at TomoApp.com, Apple App Store and Google Play Store.
 
-This is the end of today’s video on our review on Tomo Coin ICO. If you have the information useful and would like more similar videos, remember to spam the like button and subscribe. From the entire Bite Crypto team, here’s wishing everyone a happy lunar new year.
+3. **Tomowallet:**
+- Tomowallet is a secure and user-friendly wallet made by the team. It is designed for users of the Tomo network to store their Tomocoins, although it is also compatible with Ethereum and other coins and tokens. Users will be able to safely send TomoCoin, messages and other coins and tokens to friends and contacts via this wallet. There is also API integration enabled for 3rd party bots and applications. The wallet was designed with the idea of “bridging the gap between a cryptocurrency enthusiast and a mainstream user”, and their goal is to create an easy-to-use wallet that would increase the adoption rate of cryptocurrencies. A demo of the wallet can be found at https://wallet.tomocoin.io/
+
+### Team
+Core Members:
+- Long Vuong, Founder & CEO
+-- Co-founder and previous project lead of the successful NEM (New Economy Movement)
+
+- Son Nguyen, CTO & Director of Engineering
+-- Experienced and accomplished technical leader, founder of the Blockchain Developer group with more than 800 active developers.
+
+##### Notable Advisors:
+
+- Min Kim -  Head of biz dev, Bee Token
+- Roger Lim - Director, Qlink Foundation
+- Antoine C. Najjarin - Head of strategy & operations, Devery
+- Hoang Nguyen - Co-founder & VP, WeTrust
+
+We believe that the team’s top members are solid and have the necessary expertise to develop the project. The advisors are part of other projects which have been relatively successful as well, and should provide good guidance and connections for the Tomo team. The project has attracted notable investors such as NEO, which should further strengthen investor confidence in the project.
+
+
+### Roadmap
+
+Beta version of TomoApp has already been released on Q3 2017
+
+Testnet of TomoChain is already completed on Q4 2017
+
+Official release of TomoApp and TomoWallet on Q3 2018
+
+Official release of TomoChain on Q4 2018
+
+Tight but achievable roadmap as the products are already at the beta testing stage
+
+
+### Positive
+
+We like that there is already a working beta for each component of the Tomo ecosystem, and believe that Tomochain is a good solution for Ethereum’s scalability issues.
+Tomochain’s masternodes(to be implemented in 2019) will incentivize the staking of coins, driving the value of Tomocoin up.
+Experienced team members with an leader who was the co-founder and project-lead for the NEM project, which is currently one of the top 15 cryptocurrency by market cap.
+They have gained interest from renowned parties such as NEO, which should help them in terms of getting partnerships, especially those on the NEO network
+Considering the scope of the project and the pedigree of team members and advisors, the hardcap of this project is extremely low and token distribution is fairly reasonable. Tokens reserved for team are only 15%, with a 4 year vesting period. This shows that the team has faith in this project and are working for the long-term.
+
+### Negative
+
+POA consensus might lead to centralization of the network. However, we believe this will be mitigated as the team plans to switch to masternodes sometime in 2019. If they do manage to do this, we would view this as a positive, as the initial use of PoA consensus will help to prevent sybil attacks when the network has a relatively smaller number of users, and the switch to masternodes in future will allow for better decentralization when the network has grown.
+
+There are concerns on the direction of TomoChain in the long run if Ethereum manages to succesfully solve its scaling problems via sharding/plasma. In such a scenario, Tomochain would have no advantage over Ethereum(in terms of transaction speed). The team would need to work hard to grow the Tomo ecosystem so as to differentiate itself from the Ethereum network if such a scenario does occur.
+
+### Conclusion
+
+We believe that this project is a good short term investment. As the Ethereum network currently faces congestion issues on a regular basis (especially during ICO periods), the Tomo ecosystem should prove to be a good solution that many projects and users would consider moving to. The experienced team, advisors and proof of a working product are good indicators that the project will be able to deliver as per their schedule. Considering the scope of the project, this project has a low hard cap and we believe it will do well.
+
+As for the long term outlook, we are neutral on this project. There is little detail available regarding the future direction of Tomocoin as their roadmap is not very extensive. Furthermore, to ensure its long term success, Tomocoin needs to have its own unique and differentiating characteristics from the Ethereum network, so as to maintain its userbase.
+
 `}
     />
   </div>
